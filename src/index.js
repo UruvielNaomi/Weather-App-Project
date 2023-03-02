e60af02d39913a1d8eba095e05de9f6479e761b3
Naomi <33360546+UruvielNaomi@users.noreply.github.com>
2023-03-02 10:06:31 +0100

    cleaned up files and js code

--- src/index.js
+++ src/index.js
@@ -51,7 +51,6 @@ function displayForecast(response){
   }
 
   function getForecast(coordinates){
-    console.log(coordinates);
     let apiKey = "96ad27349a64ea1dcdfbe6f4d458c085";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   
